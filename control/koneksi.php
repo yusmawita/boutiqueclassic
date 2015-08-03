c6bfb92675420cb77a98f17d4aae2a9913c643f3
yusmawita <yusma.wita12@gmail.com>
2015-08-03 21:18:44 +0700

    Update koneksi.php

--- control/koneksi.php
+++ control/koneksi.php
@@ -1,4 +1,4 @@
 <?php
-$koneksi=mysql_connect("103.19.208.8","adminMygfY2","MRV_uygE89Sj");
+$koneksi=mysql_connect("103.19.208.84","adminMygfY2","MRV_uygE89Sj");
 $selectDatabase = mysql_select_db ("dbxen");
 ?>
